4a0f919497eb6c09202391a26d1dc0578ed311d1
candice.fays@ymail.com <candice.fays@ymail.com>
2023-07-02 15:13:37 +0200

    Etat du projet:
    - CSS juste pour test
    - html quasi ok, voir classes et id pour le css / manque image de loose
    - JS :
    Timer ok
    Input ok
    BadLetters testées qui s'affichent ok
    Etoiles du mot, bon nombre, sans virgule, remplacées par lettre si bonne ok

---
+++ script.js
@@ -0,0 +1,164 @@
+"use strict";
+
+// TIMER
+
+let time = 20
+const timerElement = document.getElementById("timer");
+
+const ticTac = () => {
+  time = time < 10 ? "0" + time : time
+  time = time <= 0 ? 0 : time - 1
+  timerElement.innerText = `${time}`
+  if (time === 0) {
+    timerElement.innerText = "BOOOOM !!!"
+  }
+}
+// Exécute une fonction toutes les secondes
+setInterval(ticTac, 1000)
+console.log("timer : ok")
+
+// Reste à stopper le timer quand le mot est trouvé ou quand le temps est écoulé
+
+
+// RANDOM WORDS
+const words = [
+  "javascript",
+  "formation",
+  "developpeur",
+  "ecole",
+  "ordinateur",
+  "programmation",
+  "postgresql",
+  "nodejs",
+  "react",
+  "angular",
+  "python"]
+
+const randomizeWord = () => {
+  const randomIndex = Math.floor(Math.random() * words.length)
+  return words[randomIndex].toUpperCase()
+}
+
+const randomWord = randomizeWord()
+console.log("randomWord : ok - ", randomWord)
+
+
+// DISPLAY WORDS TO GUESS WITH STARS
+const wordToGuess = document.getElementById("word-to-guess")
+const wordToGuessArray = randomWord.split("")
+console.log("wordToGuessArray : ok - ", wordToGuessArray)
+
+let starArray = []
+let starArrayString = ""
+const displayWordToGuess = () => {
+  wordToGuessArray.forEach(letter => {
+    starArray.push("*")
+    starArrayString = starArray.join(" ")
+    wordToGuess.innerText = starArrayString
+  })
+}
+displayWordToGuess()
+console.log("transfo en étoiles(starArray) : ok", starArray, starArray[1])
+
+
+// INPUT LETTER
+
+const inputLetter = document.getElementById('letter');
+const badLetters = document.getElementById("bad-letters")
+let badLettersArray = []
+let letter = ""
+let errorNumber = 0
+
+inputLetter.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter' && inputLetter !== null) {
+    letter = inputLetter.value.toUpperCase();
+    console.log('Lettre saisie (letter):', letter);
+    time = 20
+
+    // BAD AND GOOD LETTERS
+
+    // si inputLetter est dans wordToGuessArray
+    if (wordToGuessArray.includes(letter)) {
+      // pour chaque lettre qui correspond on récup l'index
+      // et on remplace à cet index l'étoile de starArray par la lettre
+      for (let i = 0; i < wordToGuessArray.length; i++) {
+        if (wordToGuessArray[i] === letter) {
+          starArray[i] = letter;
+          console.log("starArray[i] : ", starArray[i], "starArray",starArray, "marche pas; LETTER:", letter, "i:", i, "V/F:", wordToGuessArray[i] === letter)
+        }
+      }
+      // on injecte starArray dans html
+      starArrayString = starArray.join(" ")
+      wordToGuess.innerText = starArrayString
+      console.log("lettres bonnes: ", wordToGuessArray.includes(letter))
+    } else {
+      console.log("lettres bonnes: ", wordToGuessArray.includes(inputLetter), "mauvaises:", badLetters)
+      // sinon, on met la lettre dans badLetters
+      badLettersArray += letter
+      // on injecte badLetters dans html
+      badLetters.innerText = badLettersArray
+      // on fait avancer le display block
+    }
+  };
+  // suppr lettre du champs input
+  inputLetter.value = ""
+});
+
+
+
+
+// ACSII ART IMAGES BY ERRORS
+
+// Récupérer la première div visible
+// let divVisible = document.querySelector('div:not([style*="display: none"])');
+
+// Cacher la div visible
+// divVisible.style.display = 'none';
+
+// Trouver la div suivante
+// let divSuivante = divVisible.nextElementSibling;
+
+// Rendre la div suivante visible
+// if (divSuivante) {
+//   divSuivante.style.display = 'block';
+// }
+
+// OU
+// until 0 <= errorNumber <= 9
+// display none pour toutes les classes desert
+// sauf la desert avec id = error interpolation errorNumber
+// display block
+// quand errorNumber = 10 => function perdu
+
+
+
+// LOOOOOSE
+// si errorNumber = 10 || time === 0
+const loose = () => {
+  if (errorNumber === 10 || time === 0) {
+    console.log("perdu")
+    // display none pour toutes les classes desert, block pour la classe loose
+    // bouton try again
+  }
+}
+
+
+
+// SCORE
+// Sauvegarder le score
+localStorage.setItem('score', score);
+const resetButton = document.getElementById('resetButton');
+
+resetButton.addEventListener('click', function() {
+  // Récupérer le score du stockage web
+  const savedScore = localStorage.getItem('score');
+
+  // Réinitialiser la page
+  location.reload();
+
+  // Restaurer le score sauvegardé
+  if (savedScore) {
+    // Mettre à jour le score dans votre application
+    score = parseInt(savedScore);
+  }
+});
